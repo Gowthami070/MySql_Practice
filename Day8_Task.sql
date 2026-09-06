4ae7636306714f0316f06ea2c224b8bb2f5be382
Gowthami070 <gowthamimareedu18@gmail.com>
2026-09-06 20:30:39 +0530

    Queries on the and

---
+++ Day8_Task.sql
@@ -0,0 +1,6 @@
+Write a query to display the name and salary of employees who earn a commission of ₹1,400?
+Write a query to display all details of employees whose commission is greater than their salary?
+Write a query to display the employee number of employees who were hired before the year 1987?
+Write a query to display all details of employees working as ANALYST?
+Write a Python program using a function to generate all possible permutations of a string.
+Write a Python program using a generator function that continuously generates the next prime number starting from a given number.
